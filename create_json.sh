f561c882b81c00ee31f5db5951c7ce0cead531ad
James Byrne <jambyr@bas.ac.uk>
2024-05-02 14:57:39 +0100

    Added fair api script

--- create_json.sh
+++ create_json.sh
@@ -13,10 +13,18 @@ mkdir $DATA_DIR
 REPO_ARGS=""
 for REPO in $( cat $REPO_FILE ); do
   REPO_FILENAME="${REPO##*/}"
+  echo "Grabbing scorecard for $REPO"
   [ ! -f $DATA_DIR/${REPO_FILENAME}.json ] && scorecard --repo=$REPO --checks=Maintained,Packaging --format=json >$DATA_DIR/${REPO_FILENAME}.json
-  REPO_ARGS="$REPO_ARGS $DATA_DIR/${REPO_FILENAME}.json"
+  echo "Grabbing fair assessment for $REPO"
+  [ ! -f $DATA_DIR/${REPO_FILENAME}.fair.json ] && python src/data/fair_api.py $REPO >$DATA_DIR/${REPO_FILENAME}.fair.json
+  REPO_ARGS="$REPO_ARGS $REPO"
+  FILE_ARGS="$FILE_ARGS $DATA_DIR/${REPO_FILENAME}.json"
 done
 
+echo $REPO_ARGS
+echo $FILE_ARGS
+echo "Getting github data"
 python src/data/gh_retrieve.py $REPO_ARGS >$DATA_DIR/github.json
 
-python src/data/combine_metrics.py $DATA_DIR/github.json $REPO_ARGS >$DATA_DIR/ui.json
+echo "Combining data"
+python src/data/combine_metrics.py $DATA_DIR/github.json $FILE_ARGS >$DATA_DIR/ui.json
